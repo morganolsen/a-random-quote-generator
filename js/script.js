57c4fdee104d1cb8653ca5e4f191f2c6e45749e6
Morgan Olsen <git@morganolsen.no>
2020-09-20 21:31:03 +0200

    Added 6 quotes.

--- js/script.js
+++ js/script.js
@@ -11,6 +11,38 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes = [
+  {
+    quote: "Man is condemned to be free; because once thrown into the world, he is responsible for everything he does.",
+    source: 'Jean-Paul Sartre',
+    citation: 'Characterizations of Existentialism',
+    year: '1944',
+  },
+  {
+    quote: 'Those who are free of resentful thoughts surely find peace.',
+    source: 'Buddha'
+  },
+  {
+    quote: 'No one who lives in error is free.',
+    source: 'Euripides'
+  },
+  {
+    quote: 'A ceiling is simply a floor from below.',
+    source: 'Elon Musk',
+    citation: 'Twitter',
+    year: '2017'
+  },
+  {
+    quote: "Don't let the noise of others' opinions drown out your own inner voice.",
+    source: 'Steve Jobs',
+    citation: 'Stanford University commencement speech',
+    year: '2005'
+  },
+  {
+    quote: 'Although the world is full of suffering, it is also full of the overcoming of it.',
+    source: 'Helen Keller'
+  }
+]
 
 
 /***
